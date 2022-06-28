a10a242b96a0aa97ae3bc712af0a44d6c37c1861
dapoadedire <adedireadedapo19@gmail.com>
2022-06-28 08:17:27 +0100

    try-catch, event-listener, arrayindex

---
+++ Practice/arrays_at.js
@@ -0,0 +1,5 @@
+myArray = ['Nigerian', 'American', 'British', 'German', 'French'];
+
+console.log(myArray[0]);
+console.log(myArray.at(0));
+console.log(myArray.at(-1));
---
+++ Practice/try-catch.js
@@ -0,0 +1,7 @@
+try {
+    // Do stuff
+  } catch (error) {
+    throw new Error('Figure this out one day -', { cause: error });
+  }
+
+  
